92afd10c8e1a2e1bf7b478eae655b5049308c775
Vasil Yanakiev <vasil2000yanakiev@gmail.com>
2026-03-30 23:00:30 +0300

    validate_amount_consistency

--- validator.py
+++ validator.py
@@ -1,6 +1,6 @@
-from exceptions import DateOrderError
+from exceptions import AmountMismatchError, DateOrderError
 from models import ParsedDeed
-
+from word2number import w2n
 
 def validate_date_order(deed: ParsedDeed)->None:
     if deed.date_recorded <deed.date_signed:
@@ -8,3 +8,14 @@ def validate_date_order(deed: ParsedDeed)->None:
             deed.date_recorded.date(),deed.date_signed.date()
         )
 
+def validate_amount_consistency(deed: ParsedDeed, tolerance: float = 0.01) -> None:
+    try:
+        amount_from_words = float(_words_to_number(deed.amount_words))
+    except ValueError:
+        raise ValueError(f"Failed to convert {deed.amount_words} to number!")
+    
+    if abs(deed.amount_numeric - amount_from_words > tolerance):
+        raise AmountMismatchError(deed.amount_numeric, deed.amount_words, amount_from_words)
+    
+def _words_to_number(words: str) -> float:
+    return float(w2n.word_to_num(words))  # type: ignore[reportUnknownMemberType]
